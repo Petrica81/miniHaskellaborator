6137f90d30d6b1e1283deb6985dfea41357bf129
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-03-20 14:04:38 +0200

    lab4

--- src/Printing.hs
+++ src/Printing.hs
@@ -3,10 +3,17 @@ module Printing (showExp) where
 
 import Exp
 import Data.List (intercalate)
+import Data.Void (vacuous)
+import Parsing (natExp)
 
 showVar :: Var -> String
-showVar = undefined
+showVar v = show v
 
 showExp :: ComplexExp -> String
-showExp = undefined
-
+showExp (CX v) = showVar v
+showExp (Nat n) = show n
+showExp (CLam v cexp) = "\\" ++ showVar v ++ "->" ++ showExp cexp 
+showExp (CApp c1 c2) = "(" ++ showExp c1 ++ " " ++ showExp c2 ++ ")"
+showExp (Let v c1 c2) = "(let " ++ showVar v ++ ":=" ++ showExp c1 ++ "in" ++ showExp c2 ++ ")"
+showExp (LetRec v c1 c2) = "(letRec " ++ showVar v ++ ":=" ++ showExp c1 ++ "in" ++ showExp c2 ++ ")"
+showExp (List l) = "[]" ++  intercalate "," (map showExp l) ++ "]"
